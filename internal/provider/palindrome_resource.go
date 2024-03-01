3032e1d8fae70fcd598ea8302c4a1c33f12f0cd0
polatengin <enpolat@microsoft.com>
2024-03-01 00:33:25 +0000

    fixing diagnostic error message

--- internal/provider/palindrome_resource.go
+++ internal/provider/palindrome_resource.go
@@ -64,7 +64,7 @@ func (r *PalindromeResource) Configure(ctx context.Context, req resource.Configu
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected Resource Configure Type",
-			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+			fmt.Sprintf("Expected *api.ProviderClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
 		)
 
 		return
